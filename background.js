d8f53eaab80f9e79f5595f1eacaa36511b78a24f
Sarowar Hossain <98280301+sarowarhosen03@users.noreply.github.com>
2024-03-11 02:16:14 +0600

    Update background.js

--- background.js
+++ background.js
@@ -10,11 +10,11 @@ chrome.runtime.onInstalled.addListener(() => {
 
 //listener for context menu
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-
+    //the URL that will be added to based on the selection
     baseURL = info?.frameUrl;
     if (!baseURL.startsWith("https://github.com")) return;
 
-    const repoUrl = baseURL.replace('https://', 'https://vscode.dev/');
+    const repoUrl = baseURL.replace('https://github.com/', 'https://github.dev/');
 
     chrome.tabs.create({ url: repoUrl, index: tab.index + 1 });
 })
