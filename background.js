63dcc3faad675f681862b824cc3c9775db973aa0
Sarowar Hossain <98280301+sarowarhosen03@users.noreply.github.com>
2024-03-11 01:26:26 +0600

    Add files via upload
    project initiat with v1

---
+++ background.js
@@ -0,0 +1,20 @@
+chrome.runtime.onInstalled.addListener(() => {
+
+    //create context menu
+    chrome.contextMenus.create({
+        id: "gitToVscode",
+        title: "Open in Vscode",
+        contexts: ["all"],
+    })
+});
+
+//listener for context menu
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    //the URL that will be added to based on the selection
+    baseURL = info?.frameUrl;
+    if (!baseURL.startsWith("https://github.com")) return;
+
+    const repoUrl = baseURL.replace('https://', 'https://vscode.dev/');
+
+    chrome.tabs.create({ url: repoUrl, index: tab.index + 1 });
+})
